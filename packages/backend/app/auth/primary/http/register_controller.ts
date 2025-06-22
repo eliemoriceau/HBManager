abe66019387077c3b6599d897a9a27520d89a03b
eliemoriceau <62339484+eliemoriceau@users.noreply.github.com>
2025-06-22 11:03:46 +0200

    feat(auth): add input validation

--- packages/backend/app/auth/primary/http/register_controller.ts
+++ packages/backend/app/auth/primary/http/register_controller.ts
@@ -1,13 +1,14 @@
 import { RegisterUserUseCase } from '#auth/use_case/register_user_use_case'
 import type { HttpContext } from '@adonisjs/core/http'
 import { inject } from '@adonisjs/core'
+import { registerValidator } from '#auth/primary/http/register_validator'
 
 @inject()
 export default class RegisterController {
   constructor(private registerUserUseCase: RegisterUserUseCase) {}
 
   async handle({ request, response }: HttpContext) {
-    const { email, password } = request.only(['email', 'password'])
+    const { email, password } = await registerValidator.validate(request.body())
     try {
       const user = await this.registerUserUseCase.execute(email, password)
       return response.status(201).json({
---
+++ packages/backend/app/auth/primary/http/register_validator.ts
@@ -0,0 +1,11 @@
+import vine from '@vinejs/vine'
+import { InferInput } from '@vinejs/vine/types'
+
+export const registerValidator = vine.compile(
+  vine.object({
+    email: vine.string().email(),
+    password: vine.string().minLength(1),
+  })
+)
+
+export type RegisterValidatorOutput = InferInput<typeof registerValidator>
