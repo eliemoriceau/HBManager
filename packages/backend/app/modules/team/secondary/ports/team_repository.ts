5e497a28b7c4453bf78022b37a548dc7411bc0b7
eliemoriceau <62339484+eliemoriceau@users.noreply.github.com>
2025-06-25 20:51:44 +0200

    feat(team): add repository port and stub

---
+++ packages/backend/app/modules/team/secondary/ports/team_repository.ts
@@ -0,0 +1,39 @@
+import Team from '#team/domain/team'
+
+/**
+ * Port d'accès et de manipulation des équipes.
+ */
+export abstract class TeamRepository {
+  /**
+   * Retourne toutes les équipes.
+   */
+  abstract findAll(): Promise<Team[]>
+
+  /**
+   * Recherche une équipe par son identifiant.
+   * @param id Identifiant de l'équipe
+   */
+  abstract findById(id: string): Promise<Team | null>
+
+  /**
+   * Recherche les équipes correspondant au nom donné.
+   * La recherche peut être effectuée en ignorant la casse.
+   * @param name Nom de l'équipe recherché
+   */
+  abstract findByName(name: string): Promise<Team[]>
+
+  /**
+   * Ajoute une nouvelle équipe.
+   */
+  abstract create(team: Team): Promise<void>
+
+  /**
+   * Met à jour une équipe existante.
+   */
+  abstract update(team: Team): Promise<void>
+
+  /**
+   * Supprime une équipe par son identifiant.
+   */
+  abstract delete(id: string): Promise<void>
+}
